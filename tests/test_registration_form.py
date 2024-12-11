08eea06f78a268040da7c0cfb30155eb7f3f0af4
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-12 00:53:56 +0300

    Init commit

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -5,20 +5,7 @@ from demoqa_tests.registration_page import RegistrationPage, TableResponsive
 def test_registration_user(browser_settings):
     registration_page = RegistrationPage()
 
-    registration_page.open()
-    registration_page.fill_first_name(registered_user.first_name)
-    registration_page.fill_last_name(registered_user.last_name)
-    registration_page.fill_email(registered_user.user_email)
-    registration_page.fill_genter(registered_user.genter)
-    registration_page.fill_mobile(registered_user.user_number)
-    registration_page.fill_date_of_birth(registered_user.month, registered_user.year, registered_user.day)
-    registration_page.fill_subjects(registered_user.subjects)
-    registration_page.fill_hobbies(registered_user.hobbies)
-    registration_page.download_picture(registered_user.images)
-    registration_page.fill_current_address(registered_user.current_address)
-    registration_page.fill_state(registered_user.state)
-    registration_page.fill_city(registered_user.city)
-    registration_page.submit()
+
 
     table_responsive = TableResponsive()
 
