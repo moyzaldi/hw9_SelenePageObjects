962df3c76bcfe290e8fdb371a442fb049f2538d1
Tatiana Konischeva <moyzaldi@gmail.com>
2024-12-12 01:34:40 +0300

    Init commit

--- demoqa_tests/registration_page.py
+++ demoqa_tests/registration_page.py
@@ -2,66 +2,40 @@ import os
 
 from selene import browser, by, have
 
+from demoqa_tests.data.users import User
+
 
 class RegistrationPage:
 
     def open(self):
         browser.open('/automation-practice-form')
 
-    def fill_first_name(self, value):
-        browser.element('#firstName').type(value)
-
-    def fill_last_name(self, value):
-        browser.element('#lastName').type(value)
-
-    def fill_email(self, value):
-        browser.element('#userEmail').type(value)
-
-    def fill_mobile(self, value):
-        browser.element('#userNumber').type(value)
-
-    def fill_genter(self, value):
-        browser.element('#genterWrapper').element(by.text(value)).click()
-
-    def fill_date_of_birth(self, mm, yyyy, dd):
+    def register(self, value: User):
+        browser.element('#firstName').type(value.first_name)
+        browser.element('#lastName').type(value.last_name)
+        browser.element('#userEmail').type(value.user_email)
+        browser.element('#userNumber').type(value.user_number)
+        browser.element('#genterWrapper').element(by.text(value.genter)).click()
         browser.element('#dateOfBirthInput').click()
-        browser.element("[class='react-datepicker__month-select']").click().element(by.text(mm)).click()
-        browser.element("[class='react-datepicker__year-select']").click().element(by.text(yyyy)).click()
-        browser.element(f"[class ='react-datepicker__day react-datepicker__day--0{dd}']").click()
-
-    def fill_subjects(self, value):
-        browser.element('#subjectsInput').type(value).press_enter()
-
-    def fill_hobbies(self, value):
-        browser.element('#hobbiesWrapper').element(by.text(value)).click()
-
-    def download_picture(self, path):
-        browser.element("#uploadPicture").send_keys(os.path.abspath(f"../resources/images/{path}"))
-
-    def fill_current_address(self, value):
-        browser.element('#currentAddress').type(value)
-
-    def fill_state(self, value):
-        browser.element('#state').click().element(by.text(value)).click()
-
-    def fill_city(self, value):
-        browser.element('#city').click().element(by.text(value)).click()
-
-    def submit(self):
+        browser.element("[class='react-datepicker__month-select']").click().element(by.text(value.month)).click()
+        browser.element("[class='react-datepicker__year-select']").click().element(by.text(value.year)).click()
+        browser.element(f"[class ='react-datepicker__day react-datepicker__day--0{value.day}']").click()
+        browser.element('#subjectsInput').type(value.subjects).press_enter()
+        browser.element('#hobbiesWrapper').element(by.text(value.hobbies)).click()
+        browser.element("#uploadPicture").send_keys(os.path.abspath(f"../resources/images/{value.images}"))
+        browser.element('#currentAddress').type(value.current_address)
+        browser.element('#state').click().element(by.text(value.state)).click()
+        browser.element('#city').click().element(by.text(value.city)).click()
         browser.element('#submit').click()
 
-
-class TableResponsive:
-
-    def assert_text(self, firstName, lastName, userEmail, genter, userNumber, day, month, year, subjects, hobbies,
-                    images, currentAddress, state, city):
-        browser.element(("[class='table-responsive']")).should(have.text(f'{firstName} {lastName}'))
-        browser.element(("[class='table-responsive']")).should(have.text(userEmail))
-        browser.element(("[class='table-responsive']")).should(have.text(genter))
-        browser.element(("[class='table-responsive']")).should(have.text(userNumber))
-        browser.element(("[class='table-responsive']")).should(have.text(f'{day} {month},{year}'))
-        browser.element(("[class='table-responsive']")).should(have.text(subjects))
-        browser.element(("[class='table-responsive']")).should(have.text(hobbies))
-        browser.element(("[class='table-responsive']")).should(have.text(images))
-        browser.element(("[class='table-responsive']")).should(have.text(currentAddress))
-        browser.element(("[class='table-responsive']")).should(have.text(f'{state} {city}'))
+    def assert_text(self, value: User):
+        browser.element(("[class='table-responsive']")).should(have.text(f'{value.first_name} {value.last_name}'))
+        browser.element(("[class='table-responsive']")).should(have.text(value.last_name))
+        browser.element(("[class='table-responsive']")).should(have.text(value.genter))
+        browser.element(("[class='table-responsive']")).should(have.text(value.user_number))
+        browser.element(("[class='table-responsive']")).should(have.text(f'{value.day} {value.month},{value.year}'))
+        browser.element(("[class='table-responsive']")).should(have.text(value.subjects))
+        browser.element(("[class='table-responsive']")).should(have.text(value.hobbies))
+        browser.element(("[class='table-responsive']")).should(have.text(value.images))
+        browser.element(("[class='table-responsive']")).should(have.text(value.current_address))
+        browser.element(("[class='table-responsive']")).should(have.text(f'{value.state} {value.city}'))
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,18 +1,12 @@
 from demoqa_tests.data.users import registered_user
-from demoqa_tests.registration_page import RegistrationPage, TableResponsive
+from demoqa_tests.registration_page import RegistrationPage
 
 
-def test_registration_user(browser_settings):
+
+def test_registers_user(browser_settings):
     registration_page = RegistrationPage()
 
+    registration_page.open()
+    registration_page.register(registered_user)
+    registration_page.assert_text(registered_user)
 
-
-    table_responsive = TableResponsive()
-
-    table_responsive.assert_text(registered_user.first_name, registered_user.last_name, registered_user.user_email,
-                                 registered_user.genter, registered_user.user_number, registered_user.day,
-                                 registered_user.month,
-                                 registered_user.year, registered_user.subjects,
-                                 registered_user.hobbies,
-                                 registered_user.images, registered_user.current_address, registered_user.state,
-                                 registered_user.city)
